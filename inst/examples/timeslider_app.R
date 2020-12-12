a907530b28d6f2761e32ca54b36647155881b2f0
Sebastian Gatscha <gatscha@trafficon.eu>
2020-12-12 19:37:31 +0100

    timeslider example-app, cleanup

--- inst/examples/timeslider_app.R
+++ inst/examples/timeslider_app.R
@@ -1,31 +1,33 @@
-library(sf)
-library(geojsonsf)
 library(leaflet)
 library(leaflet.extras2)
+library(sf)
+library(geojsonsf)
 library(shiny)
 
 data <- sf::st_as_sf(leaflet::atlStorms2005[1,])
 data <- st_cast(data, "POINT")
-data$time = as.POSIXct(
-  seq.POSIXt(Sys.time() - 1000, Sys.time(), length.out = nrow(data)))
+data$time = as.POSIXct(seq.POSIXt(as.POSIXct("2010-01-01"), as.POSIXct("2020-01-01"), by = "year"))
 
-ui <- fluidPage(
-  leafletOutput("map", height = "800px"),
-  actionButton("rm", "Remove Time Slider")
-)
 
+ui <- fluidPage(leafletOutput("map", height = "600px"))
 server <- function(input, output, session) {
   output$map <- renderLeaflet({
+    opac <- runif(nrow(data), 0.5, 1)
     leaflet() %>%
       addTiles() %>%
       addTimeslider(data = data,
+                    color = colorNumeric("viridis", seq.int(nrow(data)))(seq.int(nrow(data))),
+                    opacity = opac, fillOpacity = opac,
+                    radius = sample(5:15, nrow(data), TRUE),
+                    popupOptions = popupOptions(maxWidth = 1000, closeOnClick= TRUE, closeButton = FALSE),
+                    popup = ~sprintf("Time: %s<br>Name: %s<br>MaxWind: %s<br>MinPress: %s",
+                                     time, Name, MaxWind, MinPress),
                     options = timesliderOptions(
-                      range = FALSE)) %>%
-      setView(-72, 22, 4)
-  })
-  observeEvent(input$rm, {
-    leafletProxy("map", session) %>%
-      removeTimeslider()
+                      startTimeIdx = 2,
+                      follow = 3,
+                      alwaysShowDate = TRUE,
+                      range = FALSE))
   })
 }
 shinyApp(ui, server)
+
--- inst/htmlwidgets/lfx-timeslider/leaflet.SliderControl-bindings.js
+++ inst/htmlwidgets/lfx-timeslider/leaflet.SliderControl-bindings.js
@@ -9,7 +9,7 @@ LeafletWidget.methods.addTimeslider = function(data, options, popupOptions) {
   // Add popups
   function onEachFeature(feature, layer) {
     var content = feature.properties.popup;
-    layer.bindPopup(content, popupOptions);
+    if (content) layer.bindPopup(content, popupOptions);
   };
 
   //Create a marker layer
@@ -30,10 +30,35 @@ LeafletWidget.methods.addTimeslider = function(data, options, popupOptions) {
     },
     onEachFeature: onEachFeature
   });
+
+  /*
+  // Multi Layer in layerGroup
+  var layer1 = L.geoJson(data, {
+    pointToLayer: function (feature, latlng) {
+        var geojsonMarkerOptions = {
+            radius: feature.properties.radius,
+            fillColor: feature.properties.fillColor,
+            color: feature.properties.color,
+            opacity: feature.properties.opacity,
+            weight: feature.properties.weight,
+            stroke: feature.properties.stroke,
+            fill: feature.properties.fill,
+            dashArray: feature.properties.dashArray,
+            fillOpacity: feature.properties.fillOpacity
+        };
+        return L.circleMarker(latlng, geojsonMarkerOptions);
+    },
+    onEachFeature: onEachFeature
+  });
+  var times = []
+  data.features.forEach(e=> times.push(e.properties.time))
+  layer.options.time = "1992";
+  layer1.options.time = "1993";
+  layer = L.layerGroup([layer, layer1]);
+  */
   options.layer = layer;
 
   // Init the slider
-  //debugger;
   map.sliderCntr = L.control.sliderControl(options);
   // Add the slider to the map
   map.addControl(map.sliderCntr);
