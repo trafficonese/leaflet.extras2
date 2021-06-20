dadaf093b827660b1bc581e9f3fb07423b520001
Rodrigo Basa <rad.basa@sesat-tech.com>
2021-06-20 19:37:10 +0800

    checks if options are passed

--- tests/testthat/test-spin.R
+++ tests/testthat/test-spin.R
@@ -7,11 +7,13 @@ test_that("spin", {
   expect_equal(deps[[length(deps)]]$name, "lfx-spin")
 
   m <- m %>% startSpinner()
-  expect_equal(m$x$calls[[length(m$x$calls)]]$method, 'spinner')
+  expect_equal(m$x$calls[[length(m$x$calls)]]$method, "spinner")
   expect_true(m$x$calls[[length(m$x$calls)]]$args[[1]])
 
   m <- m %>% stopSpinner()
-  expect_equal(m$x$calls[[length(m$x$calls)]]$method, 'spinner')
+  expect_equal(m$x$calls[[length(m$x$calls)]]$method, "spinner")
   expect_false(m$x$calls[[length(m$x$calls)]]$args[[1]])
 
+  m <- m %>% startSpinner(options = list("lines" = 7, "width" = 12))
+  expect_equal(m$x$calls[[length(m$x$calls)]]$args[[2]], list("lines" = 7, "width" = 12))
 })
