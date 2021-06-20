bfe67608c482ab222087bdd84d068533ab7c2608
Rodrigo Basa <rad.basa@sesat-tech.com>
2021-06-20 14:26:53 +0800

    start of tests for spin

---
+++ tests/testthat/test-spin.R
@@ -0,0 +1,17 @@
+test_that("spin", {
+  m <- leaflet() %>%
+    addSpinner()
+  expect_is(m, "leaflet")
+
+  deps <- findDependencies(m)
+  expect_equal(deps[[length(deps)]]$name, "lfx-spin")
+
+  m <- m %>% startSpinner()
+  expect_equal(m$x$calls[[length(m$x$calls)]]$method, 'spinner')
+  expect_true(m$x$calls[[length(m$x$calls)]]$args[[1]])
+
+  m <- m %>% stopSpinner()
+  expect_equal(m$x$calls[[length(m$x$calls)]]$method, 'spinner')
+  expect_false(m$x$calls[[length(m$x$calls)]]$args[[1]])
+
+})
