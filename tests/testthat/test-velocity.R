095240b75eef0fb34748a548594cb5eeaff822ad
Sebastian Gatscha <gatscha@trafficon.eu>
2023-04-19 19:16:19 +0200

    skip github host

--- tests/testthat/test-velocity.R
+++ tests/testthat/test-velocity.R
@@ -1,8 +1,9 @@
 library(jsonlite)
+library(curl)
 
 test_that("velocity", {
 
-  skip_if_offline(host = "https://raw.githubusercontent.com")
+  skip_if(!curl::has_internet())
 
   content <- "https://raw.githubusercontent.com/danwild/leaflet-velocity/master/demo/wind-gbr.json"
 
