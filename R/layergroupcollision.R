c339ebdc9b152e317438b82dc4eef541d8bf073f
Sebastian Gatscha <gatscha@trafficon.eu>
2024-09-02 00:09:44 +0200

    reqrite layergorupcollision.. so that it makes DivMarkers

--- R/layergroupcollision.R
+++ R/layergroupcollision.R
@@ -2,7 +2,8 @@ layergroupCollisionDependency <- function() {
   list(
     htmltools::htmlDependency(
       "lfx-layergroupcollision", version = "1.0.0",
-      src = system.file("htmlwidgets/lfx-layergroupcollision", package = "leaflet.extras2"),
+      src = system.file("htmlwidgets/lfx-layergroupcollision",
+                        package = "leaflet.extras2"),
       script = c("rbush.min.js",
                  "Leaflet.LayerGroup.Collision.js",
                  "layergroup-binding.js"),
@@ -15,13 +16,35 @@ layergroupCollisionDependency <- function() {
 #' @return A leaflet map object
 #' @export
 #'
+#' @inheritParams leaflet::addAwesomeMarkers
 #' @references \url{https://github.com/Geovation/labelgun}
 #'
 #' @name LayerroupCollision
-addLayerGroupCollision <- function(map, group=NULL, margin=5) {
-  # stopifnot("The group argument is NULL. Please define a valid group." = !is.null(group))
+addLayerGroupCollision <- function(
+    map, lng = NULL, lat = NULL, layerId = NULL, group = NULL,
+    popup = NULL, popupOptions = NULL, label = NULL,
+    labelOptions = NULL,
+    className = NULL, html = NULL,
+    options = markerOptions(), clusterOptions = NULL,
+    clusterId = NULL, margin = 5, data = getMapData(map)
+) {
 
   map$dependencies <- c(map$dependencies, layergroupCollisionDependency())
 
-  invokeMethod(map, NULL, "addLayerGroupCollision", group, margin)
+  ## Derive Points and Invoke Method ##################
+  pts <- derivePoints(
+    data, lng, lat, missing(lng), missing(lat),
+    "addLayerGroupCollision"
+  )
+  invokeMethod(
+    map, data, "addLayerGroupCollision", pts$lat, pts$lng,
+    layerId, group, options,
+    className, html,
+    popup, popupOptions,
+    label, labelOptions,
+    clusterId, clusterOptions,
+    margin,
+    getCrosstalkOptions(data)
+  ) %>%
+    expandLimits(pts$lat, pts$lng)
 }
--- inst/htmlwidgets/lfx-layergroupcollision/layergroup-binding.js
+++ inst/htmlwidgets/lfx-layergroupcollision/layergroup-binding.js
@@ -1,44 +1,112 @@
 /* global LeafletWidget, $, L, Shiny, HTMLWidgets */
-LeafletWidget.methods.addLayerGroupCollision = function(group, margin) {
-  var map = this;
+LeafletWidget.methods.addLayerGroupCollision = function(
+    lat, lng, layerId, group, options,
+    className, html,
+    popup, popupOptions, label, labelOptions,
+    clusterId, clusterOptions, divOptions,
+    crosstalkOptions) {
 
-  // Get groups as Array and Loop over all
-  var groups = Array.isArray(group) ? group : [group]
-  console.log("groups"); console.log(groups)
+    (function() {
+      var collisionLayer = L.LayerGroup.collision({margin:5});
 
-  groups.forEach(function(grp) {
-    var i = 0;
+      // Make a Dataframe
+      let df = new LeafletWidget.DataFrame()
+        .col("lat", lat)
+        .col("lng", lng)
+        .col("layerId", layerId)
+        .col("group", group)
+        .col("popup", popup)
+        .col("popupOptions", popupOptions)
+        .col("label", label)
+        .col("labelOptions", labelOptions)
+        .col("className", className)
+        .col("html", html)
+        .cbind(options)
+        .cbind(crosstalkOptions || {});
 
-
-    console.log("grp"); console.log(grp);
-
-
-    // Get the layers for the given group and call `addLabel` for each one
-    var markers = map.layerManager.getLayerGroup(grp);
-
-    //var layercollision = new L.LayerGroup.collision(markers)
-
-    var collisionLayer = L.LayerGroup.collision({margin:margin});
-    collisionLayer.addLayer(markers);
-    collisionLayer.addTo(map);
-
-/*
-    // If the marker group exists add events
-    if (markers) {
-      // Call `resetLabels` when a layer with the group name is added (LayerControl)
-      map.on("overlayadd", function(layer){
-        if (layer.name === grp) {
+      // Add Cluster
+      let clusterGroup = this.layerManager.getLayer("cluster", clusterId),
+        cluster = clusterOptions !== null;
+      if (cluster && !clusterGroup) {
+        clusterGroup = L.markerClusterGroup.layerSupport(clusterOptions);
+        if(clusterOptions.freezeAtZoom) {
+          let freezeAtZoom = clusterOptions.freezeAtZoom;
+          delete clusterOptions.freezeAtZoom;
+          clusterGroup.freezeAtZoom(freezeAtZoom);
         }
-      });
-      // Call `resetLabels` after zooming only when the group is visible
-      map.on("zoomend", function(e){
-        if (map.layerManager.getVisibleGroups().indexOf(markers.groupname) > -1) {
+        clusterGroup.clusterLayerStore = new LeafletWidget.ClusterLayerStore(clusterGroup);
+      }
+      let extraInfo = cluster ? { clusterId: clusterId } : {};
+
+      for (let i = 0; i < df.nrow(); i++) {
+        if($.isNumeric(df.get(i, "lat")) && $.isNumeric(df.get(i, "lng"))) {
+          (function() {
+
+            let thisId = df.get(i, "layerId");
+            let thisGroup = cluster ? null : df.get(i, "group");
+
+            // Create a new marker with DivIcon
+            var divIconOptions = Object.assign({}, divOptions, {
+                className: df.get(i, "className"),
+                html: df.get(i, "html")
+            });
+            var divmarker = new L.Marker(
+              [df.get(i, "lat"), df.get(i, "lng")],
+              Object.assign({}, options, {
+                  icon: new L.DivIcon(divIconOptions)
+              }));
+
+            collisionLayer.addLayer(marker);
+
+            if (cluster) {
+              clusterGroup.clusterLayerStore.add(divmarker, thisId);
+            } else {
+              this.layerManager.addLayer(divmarker, "marker", thisId, thisGroup, df.get(i, "ctGroup", true), df.get(i, "ctKey", true));
+            }
+
+            // Bind popup to the marker if popup content is provided
+            let popup = df.get(i, "popup");
+            let popupOptions = df.get(i, "popupOptions");
+            if (popup !== null) {
+              if (popupOptions !== null){
+                divmarker.bindPopup(popup, popupOptions);
+              } else {
+                divmarker.bindPopup(popup);
+              }
+            }
+
+            // Assign label (tooltip) to marker if label content is provided
+            let label = df.get(i, "label");
+            let labelOptions = df.get(i, "labelOptions");
+            if (label !== null) {
+              if (labelOptions !== null) {
+                if(labelOptions.permanent) {
+                  divmarker.bindTooltip(label, labelOptions).openTooltip();
+                } else {
+                  divmarker.bindTooltip(label, labelOptions);
+                }
+              } else {
+                divmarker.bindTooltip(label);
+              }
+            }
+
+            // Add the marker to the map's layer manager
+            this.layerManager.addLayer(divmarker, "marker", thisId, thisGroup);
+
+            // Add Listener
+            divmarker.on("click", LeafletWidget.methods.mouseHandler(this.id, thisId, thisGroup, "marker_click", extraInfo), this);
+            divmarker.on("mouseover", LeafletWidget.methods.mouseHandler(this.id, thisId, thisGroup, "marker_mouseover", extraInfo), this);
+            divmarker.on("mouseout", LeafletWidget.methods.mouseHandler(this.id, thisId, thisGroup, "marker_mouseout", extraInfo), this);
+            divmarker.on("dragend", LeafletWidget.methods.mouseHandler(this.id, thisId, thisGroup, "marker_dragend", extraInfo), this);
+
+          }).call(this);
         }
-      });
+      }
+      if (cluster) {
+        this.layerManager.addLayer(clusterGroup, "cluster", clusterId, group);
+      }
+      collisionLayer.addTo(this);
+  }).call(this);
 
-    }
-    */
-
-  })
 };
 
